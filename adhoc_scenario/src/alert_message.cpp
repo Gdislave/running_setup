e252562282d177971e33cf88fdc8ad266ccaa4e6
Marcel Mann <none@of.your>
2017-08-26 18:40:51 +0200

    played with params... more important is alert_message.cpp, now using adhoc_package datatype for transmitting information.still need position information etc...

--- adhoc_scenario/src/alert_message.cpp
+++ adhoc_scenario/src/alert_message.cpp
@@ -1,10 +1,12 @@
 #include "ros/ros.h"
 #include "ros/topic.h"
 #include "adhoc_customize/include.h"
+#include "adhoc_customize/defines.h"
 #include "adhoc_communication/functions.h"
 #include "adhoc_communication/SendString.h"
 #include "adhoc_communication/RecvString.h"
 #include <adhoc_tests/FilenameService.h>
+#include "adhoc_communication/MmRobotPosition.h"
 #include <iostream> 
 #include <fstream>
 #include <string> 
@@ -12,19 +14,25 @@
 
 
 adhoc_customize::Rectangle rectangle;
+adhoc_communication::MmRobotPosition current_pos;
 
 
 int main (int argc, char **argv){
 	
-	ros::init(argc, argv, "adhoc_sender1");
+	ros::init(argc, argv, "alert_message");
 	ros::NodeHandle nh; 
 
 	// get Parameters and print INFO
 	int loop;
-	std::string dst_car, pos;
-	nh.getParam("/rect/dst_car", dst_car);
-	nh.getParam("/rect/loop", loop);
-	ROS_INFO("loop [%d]; Dest: [%s]", loop, dst_car.c_str());
+	std::string dst_car, src_car;
+	dst_car = "pses-car6";
+	src_car = "pses-car2";
+	loop = 10;
+	/*nh.getParam("/adhoc_scenario_params/dst_car", dst_car);
+	nh.getParam("/adhoc_scenario_params/loop", loop);
+	nh.getParam("/adhoc_scenario_params/src_car", src_car); */
+	ROS_INFO("loop [%d]; Dest: [%s] ; Source: [%s]", loop, dst_car.c_str(), src_car.c_str());
+	current_pos.src_robot = src_car; 
 	
 	ros::Rate loop_rate(1);
 
@@ -36,7 +44,9 @@ int main (int argc, char **argv){
 		// send Rectangle
 		rectangle.length = i;
 		rectangle.width = i;
+		current_pos.position.pose.position.x = 10.0;
 		adhoc_communication::sendMessage(rectangle, FRAME_DATA_TYPE_RECTANGLE, dst_car, "t_rectangle");
+		adhoc_communication::sendMessage(current_pos, FRAME_DATA_TYPE_POSITION, dst_car, "traffic_light_position");
 		loop_rate.sleep();
 	}	
 	return 1;
