54e20681f5dd00e5267d45c952a77cdb76a9128a
user name <none@of.your>
2017-09-11 21:09:46 +0200

    added messagetype and publisher, to publish the message from adhoc_network to internal purpose. created testmessage to get things workings, need edit to prior state.

--- scenario_handler/src/adhoc_listener.cpp
+++ scenario_handler/src/adhoc_listener.cpp
@@ -12,6 +12,7 @@
 #include <unordered_map>
 #include <tf/transform_listener.h>
 #include <iostream>
+#include "scenario_handler/adhoc_reaction.h"
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
@@ -24,8 +25,6 @@ void chatterCallback(const std_msgs::String::ConstPtr& msg)
 */
 
 //Subscriber Callbacks
-
-
 void traffic_light_positionCallback(const adhoc_communication::MmRobotPosition::ConstPtr& tfl_pos_ptr, std::unordered_map<std::string, geometry_msgs::Pose>* pose_map)
 {
 	(*pose_map)[tfl_pos_ptr->src_robot].orientation = tfl_pos_ptr->position.pose.orientation;
@@ -56,7 +55,9 @@ void traffic_light_positionCallback(const adhoc_communication::MmRobotPosition::
 	//ROS_INFO("Position via asterisk,dereference *,  I heard: [%s]", tfl_pos_obj.position.pose.position.x);
 }
 */
-
+bool rdy_for_pub(int &a, int &b){
+  return true;
+}
 
 int main(int argc, char **argv)
 {
@@ -66,10 +67,16 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "adhoc_listener");
   ros::NodeHandle nh;
   adhoc_communication::MmRobotPosition tfl_pos_obj;
+  scenario_handler::adhoc_reaction test_object;
+  ////ros::Publisher commands_pub = nh.advertise<std_msgs::String>("adhoc_message_commands", 10);
+  ros::Publisher adhoc_rct_pub = nh.advertise<scenario_handler::adhoc_reaction>("adhoc_rct_message",10);
   //geometry_msgs::Quaternion quaternion;
   //int quaternion[4] = {0,0,0,0};
   std::unordered_map<std::string, geometry_msgs::Pose> pose_map;
-
+  //init values for delta calculation
+  double time_a = ros::Time::now().toSec(), time_b, delta;
+  delta = time_b = time_a;
+  //time_a = ;
 
   MoveBaseClient ac("move_base", true);
   while(!ac.waitForServer(ros::Duration(5.0))){
@@ -80,14 +87,37 @@ int main(int argc, char **argv)
 
   ros::Subscriber tfp_sub = nh.subscribe<adhoc_communication::MmRobotPosition>("traffic_light_position", 10, std::bind(traffic_light_positionCallback, std::placeholders::_1, &pose_map));
   ros::Rate loop_rate(10);
+  ////std_msgs::String msg_adhoc_command;
+  std::stringstream ss;
+  ss << "pses_car2";
+  ////msg_adhoc_command.data = ss.str();
+  test_object.source = ss.str();
+  test_object.message_type = "SOS";
+  test_object.position.x = 12.5;
+  test_object.position.y = -3.5;
+  test_object.position.z = 0;
+  test_object.messagenumber = 0;
+
 
    while(ros::ok()) {
 
    	//for (auto& x: pose_map) {
     //std::cout << x.first << ": " << x.second.position.x << std::endl;
   	//}
-  	
 
+    if((time_b - time_a) > 1){
+      ROS_INFO("a second has passed");
+      time_a = ros::Time::now().toSec();
+      ////commands_pub.publish(msg_adhoc_command);
+      ////ROS_INFO("just published the resue message for car : %s", msg_adhoc_command.data.c_str());
+      adhoc_rct_pub.publish(test_object);
+    }
+    time_b = ros::Time::now().toSec();
+
+    //
+    /*
+     * THIS IS JUST COMMENTED BECAUSE OF MESSAGE TESTING
+     *
   	if ( pose_map.find("pses-car2") == pose_map.end())
       {std::cout << "not found" << std::endl;}
   	else {
@@ -97,7 +127,8 @@ int main(int argc, char **argv)
   		ROS_INFO("Sending goal");
 		ac.waitForResult();
 
-  	}
+    }
+    */
 
   //	
 	
