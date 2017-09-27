7089c9d521152ea10c09e945b0a09816f76de0e8
user name <none@of.your>
2017-09-27 20:01:13 +0200

    created RobotPosition Object and defined values

--- adhoc_message_creater/src/adhoc_publisher.cpp
+++ adhoc_message_creater/src/adhoc_publisher.cpp
@@ -3,6 +3,24 @@
 #include "adhoc_communication/functions.h"
 #include "adhoc_communication/MmRobotPosition.h"
 
+
+//#include "ros/topic.h"
+//#include "adhoc_customize/include.h"
+#include "adhoc_customize/defines.h"
+//#include "adhoc_communication/functions.h"
+//#include "adhoc_communication/SendString.h"
+
+
+//#include "adhoc_communication/RecvString.h"
+//#include <adhoc_tests/FilenameService.h>
+//#include "adhoc_communication/MmRobotPosition.h"
+//#include "nav_msgs/Odometry.h"
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//#include <sys/time.h>
+//#include <tf/transform_listener.h>
+
 //#include "ros/ros.h"
 //#include "ros/topic.h"
 //#include "adhoc_customize/include.h"
@@ -23,12 +41,12 @@ int main (int argc, char **argv){
   ros::NodeHandle nodehandler;
   //ros::Publisher confPub = nodehandler.advertise<std_msgs::String>("t_filename", 1000, false);
 
-  rectangle.length = 10;
-  rectangle.width  = 20;
+  //rectangle.length = 10;
+  //rectangle.width  = 20;
   std::string dst_car = "pses-car6";
 
 
-  ROS_INFO("length [%d]; width: [%d]", rectangle.length , rectangle.width);
+  //ROS_INFO("length [%d]; width: [%d]", rectangle.length , rectangle.width);
   ros::Rate loop_rate(1);
 
   int i = 0;
@@ -41,11 +59,14 @@ int main (int argc, char **argv){
   current_pos.position.pose.position.y = 0;
   current_pos.position.pose.position.z = 0;
 
-  current_pos.pose.pose.orientation.x = 0.0;
-  current_pos.pose.pose.orientation.y = 0.0;
-  current_pos.pose.pose.orientation.z = 0.0;
-  current_pos.pose.pose.orientation.w = 1.0;
-  
+  current_pos.position.header.stamp = ros::Time::now();
+  current_pos.position.header.frame_id = "base_footprint";
+
+  current_pos.position.pose.orientation.x = 0.0;
+  current_pos.position.pose.orientation.y = 0.0;
+  current_pos.position.pose.orientation.z = 0.0;
+  current_pos.position.pose.orientation.w = 1.0;
+
   while(ros::ok() && i<loop){
     i++;
 
