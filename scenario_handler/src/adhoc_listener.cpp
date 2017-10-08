8326370c2c9a7f8e7ebd0e2786e347777014e71a
user name <none@of.your>
2017-10-08 13:31:47 +0200

    changed alignment

--- scenario_handler/src/adhoc_listener.cpp
+++ scenario_handler/src/adhoc_listener.cpp
@@ -79,7 +79,8 @@ int main(int argc, char **argv)
   //time_a = ;
 
   MoveBaseClient ac("move_base", true);
-  while(!ac.waitForServer(ros::Duration(5.0))){
+  while(!ac.waitForServer(ros::Duration(5.0)))
+  {
       ROS_INFO("Waiting for the move_base action server to come up");
   }
 
