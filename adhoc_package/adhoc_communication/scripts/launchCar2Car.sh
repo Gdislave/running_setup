db794f75df3fcb19734ec2963af5a60dea65993b
pses <none@of.your>
2017-10-10 12:32:08 +0200

    changed name of script

---
+++ adhoc_package/adhoc_communication/scripts/launchCar2Car.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo rfkill unblock wifi
+sudo service network-manager stop
+sudo ifconfig wlp2s0 down 
+ sudo iwconfig wlp2s0 mode ad-hoc
+ sudo iwconfig wlp2s0 essid cars
+ sudo iwconfig wlp2s0 ap fe:ed:de:ad:be:ef
+sudo ifconfig wlp2s0 up
+ rosrun adhoc_communication adhoc_communication_node
